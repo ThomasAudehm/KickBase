642d4893f2d7b4b1d78f05318309f16c1eb963f4
Thomas Audehm <thomas.audehm@byteclub.rocks>
2026-08-03 21:51:55 +0200

    added user secrets

--- src/KickbaseExport/Program.cs
+++ src/KickbaseExport/Program.cs
@@ -1,10 +1,15 @@
 ﻿using KickBase.Export;
 using KickBase.Api;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
 var services = builder.Services;
+var configuration = builder.Configuration;
+#if DEBUG
+configuration.AddUserSecrets<Program>();
+#endif
 services.AddHostedService<KickBaseService>();
 services.AddOptions<KickBaseApiOptions>()
     .BindConfiguration(KickBaseApiOptions.Section)
